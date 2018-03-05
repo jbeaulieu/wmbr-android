9438c3649c31d70b879152c2e163c5b3c9c5568d
Jon Beaulieu <jon@jonbeaulieu.com>
2018-03-05 15:20:50 -0600

    Add navigation drawer icons
    What it says on the tin

--- app/src/main/java/com/jbproductions/wmbr/NavigationActivity.java
+++ app/src/main/java/com/jbproductions/wmbr/NavigationActivity.java
@@ -80,17 +80,17 @@ public class NavigationActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_live_stream) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_schedule) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_archives) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_event_cal) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_connect) {
+
+        } else if (id == R.id.nav_settings) {
 
         }
 
