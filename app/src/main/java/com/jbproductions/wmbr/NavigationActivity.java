c8a88c6ba732224f93febf30e092cda68de95362
Jon Beaulieu <jon@jonbeaulieu.com>
2018-03-17 14:42:44 -0500

    Audio stream working
    Implemented button to start audio stream. Currently can log show title from stream metadata, but unable to use in app without crashing. Possible fix by moving to ASyncTask

--- app/src/main/java/com/jbproductions/wmbr/NavigationActivity.java
+++ app/src/main/java/com/jbproductions/wmbr/NavigationActivity.java
@@ -1,6 +1,7 @@
 package com.jbproductions.wmbr;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -25,6 +26,9 @@ public class NavigationActivity extends AppCompatActivity
         setContentView(R.layout.activity_navigation);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/jbproductions/wmbr/StreamFragment.java
+++ app/src/main/java/com/jbproductions/wmbr/StreamFragment.java
@@ -1,10 +1,20 @@
 package com.jbproductions.wmbr;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.jbproductions.wmbr.IcyStreamMeta;
 
 public class StreamFragment extends Fragment {
 
@@ -12,11 +22,69 @@ public class StreamFragment extends Fragment {
         // Required empty public constructor
     }
 
+    final MediaPlayer mp = new MediaPlayer();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stream, container, false);
+        View view =  inflater.inflate(R.layout.fragment_stream, container, false);
+
+        final Button streamButton = (Button) view.findViewById(R.id.button);
+        final TextView showTitleTextView = (TextView) view.findViewById(R.id.showTitle);
+
+        streamButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    mp.setDataSource("http://wmbr.org:8000/hi");
+                } catch (IllegalArgumentException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IllegalStateException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                try {
+                    mp.prepare();
+                    mp.start();
+                } catch (IllegalStateException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                String showTitle = "SHOWTITLE";
+
+                try {
+                    URL wmbr = new URL("http://wmbr.org:8000/hi");
+                    IcyStreamMeta streamMeta;
+                    streamMeta = new IcyStreamMeta(wmbr);
+
+                    try {
+                        streamMeta.refreshMeta();
+                        showTitle = streamMeta.getTitle().toString();
+                        showTitleTextView.setText(showTitle);
+                        Log.e("mytag", showTitle);
+                        //showTitleTextView.setText(showTitle);
+                        Log.e("mytag", streamMeta.getTitle());
+                    } catch (IOException e) {
+                        showTitleTextView.setText("Unable to get title metadata");
+                        e.printStackTrace();
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        return view;
     }
 
 }
