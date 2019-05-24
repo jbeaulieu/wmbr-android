7147a461adbf7713751eea22a87d035c876a07c6
Jon Beaulieu <jon@jonbeaulieu.com>
2019-05-24 00:06:29 -0500

    Removed default floating action button
    finally.

--- app/src/main/java/com/jbproductions/wmbr/NavigationActivity.java
+++ app/src/main/java/com/jbproductions/wmbr/NavigationActivity.java
@@ -7,8 +7,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -75,15 +73,6 @@ public class NavigationActivity extends AppCompatActivity
                             .commitNow();
                     mFragmentToSet = null;
                 }
-            }
-        });
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
         });
 
